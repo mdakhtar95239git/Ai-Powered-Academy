d8b47f7dc99fac95d2e4033264272b11b640860b
mdakhtar <mdakhtar91227@gmail.com>
2026-06-17 15:32:19 +0530

    delete sentry-example-page

--- src/app/api/sentry-example-api/route.ts
+++ src/app/api/sentry-example-api/route.ts
@@ -1,17 +1,14 @@
-import * as Sentry from "@sentry/nextjs";
-export const dynamic = "force-dynamic";
+import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
 class SentryExampleAPIError extends Error {
   constructor(message: string | undefined) {
     super(message);
     this.name = "SentryExampleAPIError";
   }
 }
-
 // A faulty API route to test Sentry's error monitoring
 export function GET() {
-  Sentry.logger.info("Sentry example API called");
-  throw new SentryExampleAPIError(
-    "This error is raised on the backend called by the example page.",
-  );
+  throw new SentryExampleAPIError("This error is raised on the backend called by the example page.");
+  return NextResponse.json({ data: "Testing Sentry Error..." });
 }
